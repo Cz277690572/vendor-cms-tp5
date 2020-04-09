fec5c5249f69b6816ea0ef8d23bd2f3046558ecb
levi <277690572.com>
2020-04-09 20:38:07 +0800

    banner

--- application/admin/controller/Banner.php
+++ application/admin/controller/Banner.php
@@ -10,12 +10,15 @@ class Banner extends BaseController
     public function index()
     {
         $this->title = 'Banner列表';
-        $this->_page($this->table);
+        $query = $this->_query($this->table)->where(['delete_time' => null]);
+        $query->page();
     }
 
-    public function detail()
+    public function edit()
     {
-
+        $this->title = '更新Banner';
+        $this->isAddMode = '0';
+        return $this->_form($this->table,'form');
     }
 
     public function remove()
