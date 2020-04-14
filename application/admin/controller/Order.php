f1b061a143a8221f2acd3941f129baa959b62d77
levi <277690572.com>
2020-04-14 19:03:07 +0800

    发货

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -71,17 +71,10 @@ class Order extends BaseController
         $time = time();
         if ($this->request->isGet())
         {
-            $this->orderStatus = [
-                ['value'=>1,'name'=>'未支付'],
-                ['value'=>2,'name'=>'已支付,待发货'],
-                ['value'=>3,'name'=>'已发货'],
-                ['value'=>4,'name'=>'已支付,但库存不足']
-            ];
             $where = ['delete_time' => null, 'status' => '1'];
             $this->expressList = Db::name('express_company')->where($where)->order('sort asc,id desc')->select();
         }
         elseif($this->request->isPost()) {
-            // 未支付的订单不能发货,已经更新过订单了，不要再刷了
             if(empty($vo)){
                 $this->error('订单不存在，请检查ID');
             }
@@ -90,8 +83,45 @@ class Order extends BaseController
                 $this->error('还没付款呢，想干嘛？ 或者你已经更新过订单了，不要再刷了');
             }
 
-            $order['status'] = OrderStatusEnum::DELIVERED;
-            $order['update_time'] = $time;
+            $this->_input([
+                'snap_address_name'       => $this->request->post('snap_address_name'),
+                'snap_address_mobile'     => $this->request->post('snap_address_mobile'),
+                'form_province'           => $this->request->post('form_province'),
+                'form_city'               => $this->request->post('form_city'),
+                'form_country'            => $this->request->post('form_country'),
+                'snap_address_detail'     => $this->request->post('snap_address_detail')
+            ], [
+                'snap_address_name' => 'require',
+                'snap_address_mobile' => 'require',
+                'form_province' => 'require',
+                'form_city' => 'require',
+                'form_country' => 'require',
+                'snap_address_detail' => 'require',
+            ], [
+                'snap_address_name.require' => '收货姓名不能为空！',
+                'snap_address_mobile.require' => '收货手机不能为空！',
+                'form_province.require' => '收货省份不能为空！',
+                'form_city.require' => '收货城市不能为空！',
+                'form_country.require' => '收货城区不能为空！',
+                'snap_address_detail.require' => '详细地址不能为空！',
+            ]);
+
+            $data = $this->request->post();
+            $snap_address['name']     = $data['snap_address_name'];
+            $snap_address['mobile']   = $data['snap_address_mobile'];
+            $snap_address['province'] = $data['form_province'];
+            $snap_address['city']     = $data['form_city'];
+            $snap_address['country']  = $data['form_country'];
+            $snap_address['detail']   = $data['snap_address_detail'];
+            $snap_address['update_time'] = $time;
+
+            $order['snap_address'] = json_encode($snap_address);
+            $order['status']       = OrderStatusEnum::DELIVERED;
+            $order['express_company_code']  = !empty($data['express_company_code']) ? $data['express_company_code'] : null;
+            $order['express_company_title'] = !empty($data['express_company_title']) ? $data['express_company_title'] : null;
+            $order['express_send_no']   = !empty($data['express_send_no']) ? $data['express_send_no'] : null;
+            $order['express_send_time'] = $time;
+            $order['update_time']       = $time;
             $res = Db::name('order')->update($order);
             if($res){
                 // 发送小程序模板消息通知
